5e558c0afe9e93a51c1a6d98fb812f9e49179740
James Gates <gatorjuice@gmail.com>
2021-12-27 08:07:18 -0600

    provide cassette for the validations

--- spec/models/github_repo_spec.rb
+++ spec/models/github_repo_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe GithubRepo, type: :model do
-  describe 'validations' do
+  describe 'validations', vcr: true do
     it { is_expected.to validate_presence_of(:organization) }
     it { is_expected.to validate_presence_of(:project) }
     it { is_expected.to validate_uniqueness_of(:project).scoped_to(:organization) }
