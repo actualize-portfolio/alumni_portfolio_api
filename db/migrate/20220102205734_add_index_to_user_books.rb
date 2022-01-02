919cd1d98099bb8f5aae14fc977b01219e2774c2
James Gates <gatorjuice@gmail.com>
2022-01-02 15:05:45 -0600

    did not mean to duplicate line

--- db/migrate/20220102205734_add_index_to_user_books.rb
+++ db/migrate/20220102205734_add_index_to_user_books.rb
@@ -3,6 +3,5 @@
 class AddIndexToUserBooks < ActiveRecord::Migration[6.1]
   def change
     add_index :user_books, %i[user_id book_id], unique: true
-    add_index :user_books, %i[user_id book_id], unique: true
   end
 end
