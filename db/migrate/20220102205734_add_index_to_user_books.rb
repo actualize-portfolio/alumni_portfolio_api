6db3384e180dff307e419c39b0102b833e1e4496
James Gates <gatorjuice@gmail.com>
2022-01-02 15:03:12 -0600

    add index to userbooks to speed up queries

---
+++ db/migrate/20220102205734_add_index_to_user_books.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+class AddIndexToUserBooks < ActiveRecord::Migration[6.1]
+  def change
+    add_index :user_books, %i[user_id book_id], unique: true
+    add_index :user_books, %i[user_id book_id], unique: true
+  end
+end
