b72987ffdb89379324f0acf7a9a450c31179dc43
Pinny Geliebter <pgibralter@gmail.com>
2022-01-03 13:47:34 -0500

    WIP not working spec tests

--- spec/requests/api/v1/crypto_exchanges*_spec.rb
+++ spec/requests/api/v1/crypto_exchanges*_spec.rb
@@ -1,7 +1,30 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
-RSpec.describe 'CryptoExchanges', type: :request do
-  describe 'GET /index' do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Api::V1::CryptoExchanges', type: :request do
+  include RequestSpecHelper
+  let!(:base) { 'BTC' }
+  let!(:quote) { 'USDC' }
+
+  let(:params) do
+    {
+      base: base,
+      quote: quote
+    }
+  end
+
+  describe 'GET#index' do
+    subject(:index_crypto_exchanges) do
+      get api_v1_crypto_exchanges_path, params: params
+    end
+
+    it 'returns http success' do
+      expect(response).to have_http_status(:success)
+    end
+
+    it 'returns bid-ask data' do
+      expect(data).to include({})
+    end
   end
 end
