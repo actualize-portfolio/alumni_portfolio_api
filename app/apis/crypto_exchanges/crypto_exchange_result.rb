df4e2b0b92455f54170dd9e81b074dd3e13f3c08
Pinny Geliebter <pgibralter@gmail.com>
2022-01-02 21:40:51 -0500

    adds crypto_exchange result calss

---
+++ app/apis/crypto_exchanges/crypto_exchange_result.rb
@@ -0,0 +1,20 @@
+# frozen_string_literal: true
+
+module CryptoExchanges
+  # Class for forming correct response
+  class CryptoExchangeResult
+    attr_accessor :bids, :asks, :exchange
+
+    def initialize
+      yield self if block_given?
+    end
+
+    def max_bid
+      bids.max
+    end
+
+    def min_ask
+      asks.min
+    end
+  end
+end
