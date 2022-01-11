ffb61511fb920eabf320f4362d451cbe06276cc7
yitzy32 <yitzybratt32@gmail.com>
2022-01-11 11:35:00 -0500

    separate method to find most recently pushed. save as hash

--- app/apis/github_api.rb
+++ app/apis/github_api.rb
@@ -14,12 +14,31 @@ class GithubApi
   def repository
     conn = Faraday.get("#{BASE_URL}#{@username}/repos")
     json_body = JSON.parse(conn.body)
-    date_array = []
-    json_body.each {|r| date_array << r["pushed_at"]}
-    last_pushed = date_array.max
+
+    find_most_recently_pushed_repo(json_body)
   end
 
   def exists?
 
+  end
+
+  private
+
+  def find_most_recently_pushed_repo(repos)
+    most_recently_pushed_repo = {}
+
+    placeholder = repos[0]["pushed_at"]
+
+    repos.each do |repo|
+      if repo["pushed_at"] > placeholder
+        placeholder = repo["pushed_at"]
+        
+        most_recently_pushed_repo[:name] = repo["name"]
+        most_recently_pushed_repo[:pushed_at] = repo["pushed_at"]
+        most_recently_pushed_repo[:language] = repo["language"]
+        
+      end
+    end
+    most_recently_pushed_repo
   end
 end
