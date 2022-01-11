a507e36776ffd2bb6a7c7f89d36952dce1fa0ec3
James Gates <gatorjuice@gmail.com>
2022-01-10 20:35:22 -0600

    added avatar test to model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -3,11 +3,22 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+  describe 'relationships' do
+    it { is_expected.to have_many(:user_books).dependent(:destroy) }
+    it { is_expected.to have_many(:books).through(:user_books) }
+  end
+
   describe 'validations' do
     it { is_expected.to validate_uniqueness_of(:username) }
     it { is_expected.to validate_presence_of(:username) }
     it { is_expected.to validate_presence_of(:password_digest) }
     it { is_expected.to validate_presence_of(:age) }
     it { is_expected.to validate_numericality_of(:age).only_integer }
+  end
+
+  describe 'avatar' do
+    subject { create(:user, :with_avatar).avatar }
+
+    it { is_expected.to be_an_instance_of(ActiveStorage::Attached::One) }
   end
 end
