2deeeb618bba3a331c69f8e4b0f8038e61572797
yitzy32 <yitzybratt32@gmail.com>
2022-01-10 22:19:45 -0500

    find most recently pushed repo

--- app/apis/github_api.rb
+++ app/apis/github_api.rb
@@ -14,6 +14,9 @@ class GithubApi
   def repository
     conn = Faraday.get("#{BASE_URL}#{@username}/repos")
     json_body = JSON.parse(conn.body)
+    date_array = []
+    json_body.each {|r| date_array << r["pushed_at"]}
+    last_pushed = date_array.max
   end
 
   def exists?
