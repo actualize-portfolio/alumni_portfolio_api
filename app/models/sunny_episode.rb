d43b63e38f71d88901e7de80775e60b6f18a5bd1
James Gates <gatorjuice@gmail.com>
2022-03-17 23:21:40 -0500

    removed debug

--- app/models/sunny_episode.rb
+++ app/models/sunny_episode.rb
@@ -5,9 +5,4 @@ class SunnyEpisode < ApplicationRecord
 
   validates :number, uniqueness: true
   validates :episode, uniqueness: { scope: :season }
-
-  def self.best
-    binding.pry
-    joins(:sunny_episode_user_rankings).where(better_episode: self)
-  end
 end
