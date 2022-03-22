9a8f6fa9006469c288e552013071b902e9d365f0
James Gates <gatorjuice@gmail.com>
2022-03-21 20:18:05 -0500

    switch around resource calls to make more sense

--- app/controllers/api/v1/sunny_episodes_controller.rb
+++ app/controllers/api/v1/sunny_episodes_controller.rb
@@ -4,7 +4,7 @@ module Api
   module V1
     class SunnyEpisodesController < ApplicationController
       def index
-        render_success(SunnyEpisode.random_two)
+        render_success(SunnyEpisode.top_ten)
       end
     end
   end
