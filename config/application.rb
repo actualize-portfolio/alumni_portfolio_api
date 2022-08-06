2f839ecf843b77237ca51cc69a152123d5955571
James Gates <gatorjuice@gmail.com>
2022-08-06 09:48:00 -0500

    updated more configuration elements

--- config/application.rb
+++ config/application.rb
@@ -13,13 +13,20 @@ module AlumniPortfolioApi
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Use sidekiq adapter for running ActiveJob Queue
+    config.active_job.queue_adapter = :sidekiq
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
     # in config/environments, which are processed later.
     #
-    config.time_zone = "Central Time (US & Canada)"
+    config.time_zone = 'Central Time (US & Canada)'
     # config.eager_load_paths << Rails.root.join("extras")
+
+    # ActionCable
+    config.action_cable.url = ENV.fetch('ACTION_CABLE_HOST', 'ws://localhost:3000/cable')
+    config.action_cable.disable_request_forgery_protection = true
 
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
