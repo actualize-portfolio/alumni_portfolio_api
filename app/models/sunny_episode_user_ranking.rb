35d43f1f4e3c520ad210a7b925c1cb826e283144
James <gatorjuice@gmail.com>
2023-02-04 11:04:05 -0600

    rubcop touchup

--- app/models/sunny_episode_user_ranking.rb
+++ app/models/sunny_episode_user_ranking.rb
@@ -1,5 +1,6 @@
 # frozen_string_literal: true
 
+# A Join model that links user ratings to a Sunny episode
 class SunnyEpisodeUserRanking < ApplicationRecord
   belongs_to :user
   belongs_to :better_episode, class_name: 'SunnyEpisode'
