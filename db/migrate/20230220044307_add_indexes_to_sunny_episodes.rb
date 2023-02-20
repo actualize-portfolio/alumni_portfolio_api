b7b5e55e71684d96088b76d0589f857457b101ba
James <gatorjuice@gmail.com>
2023-02-19 22:56:33 -0600

    changed indexes

--- db/migrate/20230220044307_add_indexes_to_sunny_episodes.rb
+++ db/migrate/20230220044307_add_indexes_to_sunny_episodes.rb
@@ -2,8 +2,8 @@
 
 class AddIndexesToSunnyEpisodes < ActiveRecord::Migration[7.0]
   def change
-    add_index :sunny_episode_user_rankings, %i[better_episode_id user_id], unique: true
-    add_index :sunny_episode_user_rankings, %i[worse_episode_id user_id], unique: true
+    add_index :sunny_episode_user_rankings, %i[better_episode_id user_id], name: 'idx_better_user'
+    add_index :sunny_episode_user_rankings, %i[worse_episode_id user_id], name: 'idx_worse_user'
     add_index :sunny_episodes, :id
   end
 end
